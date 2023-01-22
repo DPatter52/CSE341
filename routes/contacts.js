61c6d30ab50f57b73adc9ae3dbbd42474de56655
Devin Patterson <dzpatterson15@gmail.com>
2023-01-21 19:36:09 -0700

    update

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const routes = express.Router();
 
-const { getContacts } = require('../controllers/contacts');
-const { getContact } = require('../controllers/contacts');
+const { getContacts, getContact, createContact, updateContact, deleteContact } = require('../controllers/contacts');
 
 routes.get('/', getContacts);
 
 routes.get('/:id', getContact);
+
+routes.get('/', createContact);
+
+routes.get('/:id', updateContact);
+
+routes.get('/:id', deleteContact);
 
 module.exports = routes;
