ecad8a4a632fec950e6cf80732da4dd47fc36061
Devin Patterson <dzpatterson15@gmail.com>
2023-02-04 19:25:51 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/book_inventory", require("../book_inventory"));
-router.use("/checkout_info", require("../checkout_info"));
+router.use("/book_inventory", require("./books"));
+// router.use("/checkout_info", require("../checkout_info"));
 router.use("/", require("./swagger"));
 
 module.exports = router;
