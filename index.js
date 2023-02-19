81ee46770dc15eb29735cea9889d60627786ce01
Devin Patterson <dzpatterson15@gmail.com>
2023-02-18 23:05:51 -0700

    update lesson 7

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-
-
-// Google OAuth
-
-const fs = require("fs").promises;
-const path = require("path");
-const process = require("process");
-const { authenticate } = require("@google-cloud/local-auth");
-const { google } = require("googleapis");
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/contacts.readonly"];
+const SCOPES = ['https://www.googleapis.com/auth/contacts.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), "token.json");
-const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 /**
  * Reads previously authorized credentials from the save file.
@@ -42,7 +38,7 @@ async function saveCredentials(client) {
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
   const payload = JSON.stringify({
-    type: "authorized_user",
+    type: 'authorized_user',
     client_id: key.client_id,
     client_secret: key.client_secret,
     refresh_token: client.credentials.refresh_token,
@@ -69,34 +65,34 @@ async function authorize() {
   return client;
 }
 
-/**
- * Print the display name if available for 10 connections.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listConnectionNames(auth) {
-  const service = google.people({ version: "v1", auth });
-  const res = await service.people.connections.list({
-    resourceName: "people/me",
-    pageSize: 10,
-    personFields: "names,emailAddresses",
-  });
-  const connections = res.data.connections;
-  if (!connections || connections.length === 0) {
-    console.log("No connections found.");
-    return;
-  }
-  console.log("Connections:");
-  connections.forEach((person) => {
-    if (person.names && person.names.length > 0) {
-      console.log(person.names[0].displayName);
-    } else {
-      console.log("No display name found for connection.");
-    }
-  });
-}
+// /**
+//  * Print the display name if available for 10 connections.
+//  *
+//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+//  */
+// async function listConnectionNames(auth) {
+//   const service = google.people({version: 'v1', auth});
+//   const res = await service.people.connections.list({
+//     resourceName: 'people/me',
+//     pageSize: 10,
+//     personFields: 'names,emailAddresses',
+//   });
+//   const connections = res.data.connections;
+//   if (!connections || connections.length === 0) {
+//     console.log('No connections found.');
+//     return;
+//   }
+//   console.log('Connections:');
+//   connections.forEach((person) => {
+//     if (person.names && person.names.length > 0) {
+//       console.log(person.names[0].displayName);
+//     } else {
+//       console.log('No display name found for connection.');
+//     }
+//   });
+// }
 
-authorize().then(listConnectionNames).catch(console.error);
+authorize().catch(console.error);
 
 
 // startup
