e4e78c78c818ab639864903fe7b03fa9c781b831
Devin Patterson <dzpatterson15@gmail.com>
2023-02-18 23:29:05 -0700

    lesson 7 update

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
+const fs = require("fs").promises;
+const path = require("path");
+const process = require("process");
+const { authenticate } = require("@google-cloud/local-auth");
+const { google } = require("googleapis");
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/contacts.readonly'];
+const SCOPES = ["https://www.googleapis.com/auth/contacts.readonly"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
 /**
  * Reads previously authorized credentials from the save file.
@@ -38,7 +38,7 @@ async function saveCredentials(client) {
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
   const payload = JSON.stringify({
-    type: 'authorized_user',
+    type: "authorized_user",
     client_id: key.client_id,
     client_secret: key.client_secret,
     refresh_token: client.credentials.refresh_token,
@@ -65,67 +65,43 @@ async function authorize() {
   return client;
 }
 
-// /**
-//  * Print the display name if available for 10 connections.
-//  *
-//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-//  */
-// async function listConnectionNames(auth) {
-//   const service = google.people({version: 'v1', auth});
-//   const res = await service.people.connections.list({
-//     resourceName: 'people/me',
-//     pageSize: 10,
-//     personFields: 'names,emailAddresses',
-//   });
-//   const connections = res.data.connections;
-//   if (!connections || connections.length === 0) {
-//     console.log('No connections found.');
-//     return;
-//   }
-//   console.log('Connections:');
-//   connections.forEach((person) => {
-//     if (person.names && person.names.length > 0) {
-//       console.log(person.names[0].displayName);
-//     } else {
-//       console.log('No display name found for connection.');
-//     }
-//   });
-// }
-
 authorize().catch(console.error);
-
 
 // startup
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongodb = require("./db/connect");
+if (!authorize) {
+  console.log('Could not connect to authorizor.', error);
+  process.exit();
+} else {
+  const express = require("express");
+  const bodyParser = require("body-parser");
+  const mongodb = require("./db/connect");
 
-const port = process.env.PORT || 8090;
-const app = express();
+  const port = process.env.PORT || 3000;
+  const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
-  })
-  .use("/", require("./routes"));
+  app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      next();
+    })
+    .use("/", require("./routes"));
 
-// Error Handeling
-process.on("uncaughtException", (err, origin) => {
-  console.log(
-    process.stderr.fd,
-    `Caught exception: ${err}\n` + `Exception origin ${origin}`
-  );
-});
+  process.on("uncaughtException", (err, origin) => {
+    console.log(
+      process.stderr.fd,
+      `Caught exception: ${err}\n` + `Exception origin ${origin}`
+    );
+  });
 
-mongodb.initDb((error, mongodb) => {
-  if (error) {
-    console.log("There was an issue connecting to the database!", error);
-    process.exit();
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}!`);
-  }
-});
+  mongodb.initDb((error, mongodb) => {
+    if (error) {
+      console.log("There was an issue connecting to the database!", error);
+      process.exit();
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}!`);
+    }
+  });
+}
