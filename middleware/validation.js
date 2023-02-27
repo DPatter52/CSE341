15acfa2e0289a7e8af4395af7c1a15bb27ab27af
Devin Patterson <dzpatterson15@gmail.com>
2023-02-27 16:48:53 -0700

    update

--- middleware/validation.js
+++ middleware/validation.js
@@ -24,33 +24,40 @@ const { check } = require("express-validator");
 //   };
 
 exports.saveBook = [
-  check("title", "Title is required").not().isEmpty(),
+  check("title", "Book title is required").not().isEmpty(),
   check("author", "Author is required").not().isEmpty(),
   check("pages", "Number of pages is required").not().isEmpty(),
   check("genre", "Genre type is required").not().isEmpty(),
   check("publishYear", "Publish year is required").not().isEmpty(),
-  check("invCount", "Inventory number is required").not().isEmpty(),
+  check("invCount", "Inventory number is required").not().isEmpty()
 ]
 
-const saveRenter = (req, res, next) => {
-  const validationReq = {
-    name: "required|string",
-    birthday: "required|string",
-    title: "required|string",
-    checkout: "required|string",
-  };
+exports.saveRenter = [
+  check("name", "Name is required").not().isEmpty(),
+  check("birthday", "Birth date is required").not().isEmpty(),
+  check("title", "Book title is required").not().isEmpty(),
+  check("checkout", "Checkout date is required").not().isEmpty(),
+]
 
-  check(req.body, validationReq, {}, (err, status) => {
-    if (!status) {
-      res.status(412).send({
-        success: false,
-        message: "Validation Failed",
-        data: err,
-      });
-    } else {
-      next();
-    }
-  });
-};
+// const saveRenter = (req, res, next) => {
+//   const validationReq = {
+//     name: "required|string",
+//     birthday: "required|string",
+//     title: "required|string",
+//     checkout: "required|string",
+//   };
+
+//   check(req.body, validationReq, {}, (err, status) => {
+//     if (!status) {
+//       res.status(412).send({
+//         success: false,
+//         message: "Validation Failed",
+//         data: err,
+//       });
+//     } else {
+//       next();
+//     }
+//   });
+// };
 
 
