e1d8cf481903b1eb035d781ebdc5a7107357ff7a
Devin Patterson <dzpatterson15@gmail.com>
2023-02-27 16:07:34 -0700

    validation update

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,27 +1,36 @@
 const { check } = require("express-validator");
 
-const saveBook = (req, res, next) => {
-  const validationReq = {
-    title: "required|string",
-    author: "required|string",
-    pages: "required|string",
-    genre: "required|string",
-    puplishYear: "required|string",
-    invCount: "required|string",
-  };
+// const saveBook = (req, res, next) => {
+//   const validationReq = {
+//     title: "required|string",
+//     author: "required|string",
+//     pages: "required|string",
+//     genre: "required|string",
+//     puplishYear: "required|string",
+//     invCount: "required|string",
+//   };
 
-  check(req.body, validationReq, {}, (err, status) => {
-    if (!status) {
-      res.status(412).send({
-        success: false,
-        message: "Validation Failed",
-        data: err,
-      });
-    } else {
-      next();
-    }
-  });
-};
+//     check(req.body, validationReq, {}, (err, status) => {
+//       if (!status) {
+//         res.status(412).send({
+//           success: false,
+//           message: "Validation Failed",
+//           data: err,
+//         });
+//       } else {
+//         next();
+//       }
+//     });
+//   };
+
+exports.saveBook = [
+  check("title", "Title is required").not().isEmpty(),
+  check("author", "Author is required").not().isEmpty(),
+  check("pages", "Number of pages is required").not().isEmpty(),
+  check("genre", "Genre type is required").not().isEmpty(),
+  check("publishYear", "Publish year is required").not().isEmpty(),
+  check("invCount", "Inventory number is required").not().isEmpty(),
+]
 
 const saveRenter = (req, res, next) => {
   const validationReq = {
@@ -44,7 +53,4 @@ const saveRenter = (req, res, next) => {
   });
 };
 
-module.exports = {
-  saveBook,
-  saveRenter,
-};
+
